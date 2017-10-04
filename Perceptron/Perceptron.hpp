5c9409e4daa5f3cd42f5655a52f7ff6034c354f6
14gasher <asher.gunsay@gmail.com>
2017-10-04 17:54:11 -0600

    Perceptron comments

--- Perceptron/Perceptron.hpp
+++ Perceptron/Perceptron.hpp
@@ -16,9 +16,35 @@ private:
   float c = 0.00001;
   unsigned int inputAndWeightCount;
 public:
+  /**
+   * Constructor
+   *
+   * @param inputCount
+   */
   Perceptron(unsigned int inputCount);
+
+
+  /**
+   * Deconstructor
+   */
   ~Perceptron();
+
+
+  /**
+   * Returns true or false based on weights
+   *
+   * @param inputs
+   * @return
+   */
   int processInput(float* inputs);
+
+  /**
+   * Trains the perceptron. Compares expected vs output and changes weight according to bias
+   *
+   * @param trainInputs
+   * @param desired
+   * @return
+   */
   bool train(float* trainInputs, int desired);
 };
 
@@ -28,8 +54,28 @@ private:
   float slope;
   float yIntercept;
 public:
+  /**
+   * Constructor. This is where the training will take place.
+   *
+   * @param p
+   * @param inputCount
+   * @param desiredTrainingPoints
+   * @param m
+   * @param b
+   */
   Trainer(Perceptron& p, unsigned int inputCount, unsigned int desiredTrainingPoints, float m, float b);
+
+  /**
+   * Gets the correct answer of the given line
+   * @param x
+   * @param z
+   * @return
+   */
   int linearAnswer(float x, float z);
+
+  /**
+   * Deconstructor
+   */
   ~Trainer();
 };
 
